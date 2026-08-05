2dffc05997ded05ff7a90c4bc6550b99ed196ab8
cyh <1711351315@qq.com>
2026-08-05 23:50:24 +0800

    example演示top-p采样

--- example.py
+++ example.py
@@ -26,7 +26,7 @@ def main():
     tokenizer = AutoTokenizer.from_pretrained(path)
     llm = LLM(path, enforce_eager=True, tensor_parallel_size=1)
 
-    sampling_params = SamplingParams(temperature=0.6, max_tokens=256)
+    sampling_params = SamplingParams(temperature=0.6, top_p=0.9, max_tokens=256)
     prompts = [
         "introduce yourself",
         "list all prime numbers within 100",
